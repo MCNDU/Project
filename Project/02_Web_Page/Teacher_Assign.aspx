dc8ffd9efd6201db050e4efd54fe7cc116c1addf
Zhi-Yu Su <bobbob2367149@gmail.com>
2019-05-28 10:29:09 +0800

    更新

---
+++ Project/02_Web_Page/Teacher_Assign.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Teacher_Assign.aspx.cs" Inherits="Project._02_Web_Page.Teacher_Assign" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Project/02_Web_Page/Teacher_Assign.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project._02_Web_Page {
+    public partial class Teacher_Assign : System.Web.UI.Page {
+        protected void Page_Load (object sender, EventArgs e) {
+
+        }
+    }
+
+    class Node {
+
+        //建立鏈結串列
+        static Node [] Teacher;
+        static LinkedList<Node> Teacher_List = new LinkedList<Node> (Teacher);
+
+        //鏈結串列內資料
+        String Teacher_Name;
+        float Choose_Time_Remain ;
+
+        //計算用資料
+        int Teacher_Amount = 5;
+        int Student_Amount = 10;
+
+        //Choose_Time_Remain = Teacher_Amount / Student_Amount;
+
+        Node (String Teacher_Name, int Choose_Time_Remain) {
+            this.Teacher_Name = Teacher_Name;
+            this.Choose_Time_Remain = Choose_Time_Remain;
+        }
+
+        public static void Display (string Teacher_Name) {
+            foreach (Node Read in Teacher_List) {
+                
+            }
+            //網頁上條列老師姓名
+        }
+
+        public static void Add (string Teacher_Name) {
+        //    Node insert = new Node (Teacher_Name, Choose_Time_Remain);
+        }
+    }
+}
