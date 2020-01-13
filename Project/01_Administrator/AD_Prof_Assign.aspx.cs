897afedaf6798bc1f88ab6f2c61ec5e6459d0c1f
Zhi-Yu Su <bobbob2367149@gmail.com>
2020-01-13 10:19:33 +0800

    已完成教師編號轉換功能

--- Project/01_Administrator/AD_Prof_Assign.aspx.cs
+++ Project/01_Administrator/AD_Prof_Assign.aspx.cs
@@ -10,9 +10,9 @@ using System.Data.SqlClient;
 namespace Project._01_Administrator {
     public partial class AD_Prof_Assign : System.Web.UI.Page {
 
-        // 於方法內接收外部傳入的數值，臨時紀錄用
-        DropDownList Selected_Batch_DDL;
-        GridView Selected_GridView_GV;
+        // 連結資料庫的字串
+        String ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                        "AttachDBFilename=|DataDirectory|Project_Db.mdf";
 
         // 頁面載入時，觸發此方法
         protected void Page_Load (object sender, EventArgs e) {
@@ -23,15 +23,15 @@ namespace Project._01_Administrator {
         protected void Student_GV_SelectedIndexChanged (object sender, EventArgs e) {
 
             // 承接自控制項傳入的資料以精簡程式碼
-            Selected_GridView_GV = (GridView) sender;
 
-            string ID = Selected_GridView_GV.SelectedValue.ToString ();
+            string ID = Student_GV.SelectedValue.ToString ();
 
+            // TO BE CONTINUE ==>>
             // 傳送資料至Studnet_DV以顯示被選擇的該名研究生資料
             Student_SDS_ForDV.SelectCommand =
             "SELECT ID, Name, Ch_Title, First_Commit, Second_Commit, Third_Commit, First_Prof FROM Student WHERE ID = " + ID + ";";
             Student_DV.DataBind ();
-
+            Student_DV.Rows[3].Cells[1].Text = Student_GV.SelectedRow.Cells[5].Text;
             // 傳送資料至Teacher_DV以顯示分配給該名研究生之審查委員資料
             // (依據1、2、3順序放置於Teacher_DV之1、2、3分頁)
 
@@ -42,19 +42,15 @@ namespace Project._01_Administrator {
         // 讓虛擬表格取得資料的方法
         private void Getdata (GridView gridView, DataTable dataTable, String queryString) {
             
-            // 連結資料庫的字串
-            String ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
-                            "AttachDBFilename=|DataDirectory|Project_Db.mdf";
-
             // 連結至資料庫並執行指令(也就是queryString)
             SqlConnection connection = new SqlConnection (ConStr);
             SqlDataAdapter adapter = new SqlDataAdapter (queryString, connection);
 
             // 將取得的資料放入dataTable
             adapter.Fill (dataTable);
-
+            
             // 如果沒有相關資料，新增三行空白列(維持版面)
-            if (dataTable.Rows.Count > 0) {
+            if (dataTable.Rows.Count == 0) {
                 for (var r = 0; r < 3; r++) {
                     dataTable.Rows.Add ();
                 }
@@ -69,25 +65,60 @@ namespace Project._01_Administrator {
         // 資料庫內，研究生指導教師、審查委員等等之資料
         // 其記錄方式均透過教師編號完成(避免有教師同名)
         // 因此在實際呈現給使用者之前，需先行轉換為與編號相對應之教師名稱
-        private void Transform (GridView gridView, DataTable dataTable, int row, int col, int range) {
-            
+        // 轉換順序：dataTable -> 資料表查詢 -> gridView
+        // 位置對應：dataTable[0][0] <=> gridView[0][2]
+        private void Transform (GridView gridView, DataTable dataTable, int col_Start, int col_End) {
+
+            int row_Sum = dataTable.Rows.Count;
+
+            // 轉換過程為迴圈(逐列逐行轉換)
+            for (int row_Current = 0; row_Current < row_Sum; row_Current++) {
+                for (int col_Current = col_Start; col_Current <= col_End; col_Current++) {
+
+                    // 如果資料為空(該研究生並沒有指導老師or審查委員)
+                    // 將轉換為空字串填入表格
+                    if ( String.IsNullOrEmpty (dataTable.Rows[row_Current][col_Current].ToString ()) ) {
+                        gridView.Rows[row_Current].Cells[col_Current + 2].Text = "";
+                    }
+                    // 如果資料不為空，進行查詢並填入結果
+                    else {
+                        // 自dataTable中抓取教師編號
+                        var PF_ID = dataTable.Rows[row_Current][col_Current];
+
+                        // 以教師編號為索引，自資料表Teacher中抓取對應資料列
+                        String Transform_queryString =
+                            "SELECT Name FROM [Teacher] WHERE ID = " + PF_ID + ";";
+
+                        SqlConnection connection = new SqlConnection (ConStr);
+                        SqlCommand command = new SqlCommand (Transform_queryString, connection);
+                        connection.Open ();
+
+                        SqlDataReader dataReader = command.ExecuteReader ();
+                        dataReader.Read ();
+
+                        // 將資料列中教師姓名放置gridview中對應的位置
+                        gridView.Rows[row_Current].Cells[col_Current + 2].Text = dataReader.GetString (0);
+                        
+                        connection.Close ();
+                    }
+                }
+            }
         }
 
         protected void Batch_DDL_SelectedIndexChanged (object sender, EventArgs e) {
             
             // 抓取使用者所選期別
-            Selected_Batch_DDL = (DropDownList) sender;
-            int Batch = int.Parse (Selected_Batch_DDL.SelectedValue);
+            int Batch = int.Parse (Batch_DDL.SelectedValue);
 
             // 取得該期研究生資料
             String ST_queryString =
-                "SELECT ID, Name, Ch_Title, First_Commit, Second_Commit, Third_Commit, First_Prof FROM [Student] WHERE Batch = " + Batch + ";";
+                "SELECT ID, Name, Ch_Title, First_Prof, Second_Commit, Third_Commit, First_Commit FROM [Student] WHERE Batch = " + Batch + ";";
             DataTable ST_dataTable = new DataTable ();
             
             Getdata (Student_GV, ST_dataTable, ST_queryString);
 
-            // 資料處理功能(教師編號轉換姓名)
-            Transform (Student_GV, ST_dataTable, 0, 5, 4);
+            // 資料轉換功能(教師編號轉換姓名)
+            Transform (Student_GV, ST_dataTable, 3, 6);
 
             // 取得該期教師資料
             String PF_queryString =
@@ -95,8 +126,6 @@ namespace Project._01_Administrator {
             DataTable PF_dataTable = new DataTable ();
             
             Getdata (Teacher_GV, PF_dataTable, PF_queryString);
-
-            this.Doc_Title_Forward.Text = this.Student_GV.Rows[0].Cells[2].Text;
         }
     }
 }
